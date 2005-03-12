7449db2ac96381b630e99f0c9a8f2451d50c5033
Matt Curland <Matt.Curland@neumont.edu>
2005-03-12 02:00:16 +0000

    Additional work on enabling F5 in the ORMModel project (modified install.bat, added project dependency so satellite builds first)
    SVN Revision: 66

--- install.bat
+++ install.bat
@@ -1,12 +1,19 @@
-copy /y "ORMModel\bin\Debug\Northface.Tools.ORM.dll" "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\PrivateAssemblies\"
-copy /y "ORMModel\bin\Debug\Northface.Tools.ORM.pdb" "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\PrivateAssemblies\"
-copy /y "ORMModelSatDll\Debug\Northface.Tools.ORMUI.dll" "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\PrivateAssemblies\1033"
-copy /y "ORMModelSatDll\Debug\Northface.Tools.ORMUI.pdb" "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\PrivateAssemblies\1033"
-copy /y "ORMModel\Shell\ProjectItems\ORMProjectItems.vsdir" "C:\Program Files\Microsoft Visual Studio 8\VC#\CSharpProjectItems"
-copy /y "ORMModel\Shell\ProjectItems\ORMModel.orm" "C:\Program Files\Microsoft Visual Studio 8\VC#\CSharpProjectItems"
-copy /y "ORMModel\Shell\ProjectItems\FactEditor.fct" "C:\Program Files\Microsoft Visual Studio 8\VC#\CSharpProjectItems"
-copy /y "ORMModel\Shell\ProjectItems\ORMProjectItems.vsdir" "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\NewFileItems"
-copy /y "ORMModel\Shell\ProjectItems\ORMModel.orm" "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\NewFileItems"
-copy /y "ORMModel\Shell\ProjectItems\FactEditor.fct" "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\NewFileItems"
-regedit /s ORMDesigner.vrg
-regedit /s FactEditor.vrg
+@echo off
+set rootPath=%1
+if '%2'=='' (
+set envPath="C:\Program Files\Microsoft Visual Studio 8\"
+) else (
+set envPath=%2
+)
+copy /y %rootPath%"ORMModel\bin\Debug\Northface.Tools.ORM.dll" %envPath%"Common7\IDE\PrivateAssemblies\"
+copy /y %rootPath%"ORMModel\bin\Debug\Northface.Tools.ORM.pdb" %envPath%"Common7\IDE\PrivateAssemblies\"
+copy /y %rootPath%"ORMModelSatDll\Debug\Northface.Tools.ORMUI.dll" %envPath%"Common7\IDE\PrivateAssemblies\1033"
+copy /y %rootPath%"ORMModelSatDll\Debug\Northface.Tools.ORMUI.pdb" %envPath%"Common7\IDE\PrivateAssemblies\1033"
+copy /y %rootPath%"ORMModel\Shell\ProjectItems\ORMProjectItems.vsdir" %envPath%"VC#\CSharpProjectItems"
+copy /y %rootPath%"ORMModel\Shell\ProjectItems\ORMModel.orm" %envPath%"VC#\CSharpProjectItems"
+copy /y %rootPath%"ORMModel\Shell\ProjectItems\FactEditor.fct" %envPath%"VC#\CSharpProjectItems"
+copy /y %rootPath%"ORMModel\Shell\ProjectItems\ORMProjectItems.vsdir" %envPath%"Common7\IDE\NewFileItems"
+copy /y %rootPath%"ORMModel\Shell\ProjectItems\ORMModel.orm" %envPath%"Common7\IDE\NewFileItems"
+copy /y %rootPath%"ORMModel\Shell\ProjectItems\FactEditor.fct" %envPath%"Common7\IDE\NewFileItems"
+regedit /s %rootPath%ORMDesigner.vrg
+regedit /s %rootPath%FactEditor.vrg
