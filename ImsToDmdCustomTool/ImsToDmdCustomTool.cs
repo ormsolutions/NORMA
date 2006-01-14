8ed3ecc3aefa812f565d45d7ba26536b4c810c2f
Matt Curland <Matt.Curland@neumont.edu>
2006-01-14 02:10:31 +0000

    Update NUImsToDmdGenerator to work without ImsToDmd.exe (stopped shipping with DSLTools RTM drop). Build solution in IMSToDmdCustomTool and run install once by hand to automatically regenerate ORMShape.dsldm when ORMShape.ims is modified. refs #168
    SVN Revision: 404

--- ImsToDmdCustomTool/ImsToDmdCustomTool.cs
+++ ImsToDmdCustomTool/ImsToDmdCustomTool.cs
@@ -7,12 +7,19 @@ using System.IO;
 using Microsoft.VisualStudio;
 using System.Text;
 using System.Diagnostics;
+using Microsoft.VisualStudio.Modeling;
+using Microsoft.VisualStudio.Modeling.Diagrams;
+using Microsoft.VisualStudio.EnterpriseTools.Corona.Concepts;
+using Microsoft.VisualStudio.EnterpriseTools.Corona.Serialization;
+using Microsoft.VisualStudio.EnterpriseTools.Phoenix.ObjectModel;
+using System.Reflection;
 
 namespace Neumont.Tools.Converters
 {
 	/// <summary>
-	/// A custom tool to shell the Microsoft-provided ImsToDmd.exe tool.
-	/// Generates an .dmd file from a .ims file when applied as a custom tool
+	/// A custom tool to replace the ImsToDmd.exe tool provided by Microsoft
+	/// in earlier drops of the DSLTools SDK.
+	/// Generates a .dsldm file from a .ims file when applied as a custom tool
 	/// on an .ims file in the Solution Explorer.
 	/// </summary>
 	[Guid("a4febd86-790b-43a9-a6e7-2813886ab0d5")]
@@ -20,7 +27,6 @@ namespace Neumont.Tools.Converters
 	{
 		#region Member Variables
 		private MsOle.IServiceProvider myServiceProvider;
-		private static string mysInstallDirectory;
 		private const string ModelFileExtension = ".dsldm";
 		#endregion // Member Variables
 		#region Constructor
@@ -39,7 +45,6 @@ namespace Neumont.Tools.Converters
 		}
 		int IVsSingleFileGenerator.Generate(string wszInputFilePath, string bstrInputFileContents, string wszDefaultNamespace, IntPtr[] rgbOutputFileContents, out uint pcbOutput, IVsGeneratorProgress pGenerateProgress)
 		{
-			string s = InstallDirectory;
 			pGenerateProgress.Progress(1, 4);
 			byte[] bytes = Encoding.UTF8.GetBytes(GenerateDmd(bstrInputFileContents));
 			pGenerateProgress.Progress(3, 4);
@@ -64,84 +69,6 @@ namespace Neumont.Tools.Converters
 		}
 		#endregion // IObjectWithSite Implementation
 		#region CustomTool Specific
-		private static object mysLockObject;
-		private static object LockObject
-		{
-			get
-			{
-				if (mysLockObject == null)
-				{
-					System.Threading.Interlocked.CompareExchange(ref mysLockObject, new object(), null);
-				}
-				return mysLockObject;
-			}
-		}
-		private string InstallDirectory
-		{
-			get
-			{
-				if (mysInstallDirectory == null)
-				{
-					lock (LockObject)
-					{
-						if (mysInstallDirectory == null)
-						{
-							Guid hierGuid = typeof(IVsHierarchy).GUID;
-							IntPtr pvHier = IntPtr.Zero;
-							IntPtr pvShell = IntPtr.Zero;
-							try
-							{
-								// The service provider we are handed is very limited, but getting
-								// the hierarchy is one of the supported services. This gets another
-								// service from the hierarchy which is then used to get the install
-								// directory for the shell. This is pushing things a bit, but is
-								// the only reliable way to get the VS shell install directory. Note
-								// that we could also look at our assembly location, but that is more
-								// fragile because the success of this tool would then depend of the relative
-								// position of the install directory, and whether or not it was placed in the gac.
-								myServiceProvider.QueryService(ref hierGuid, ref hierGuid, out pvHier);
-								if (pvHier != IntPtr.Zero)
-								{
-									IVsHierarchy hier = Marshal.GetObjectForIUnknown(pvHier) as IVsHierarchy;
-									if (hier != null)
-									{
-										MsOle.IServiceProvider spFull;
-										hier.GetSite(out spFull);
-										if (spFull != null)
-										{
-											Guid shellGuid = typeof(IVsShell).GUID;
-											spFull.QueryService(ref shellGuid, ref shellGuid, out pvShell);
-											if (pvShell != IntPtr.Zero)
-											{
-												IVsShell shellPointer = Marshal.GetObjectForIUnknown(pvShell) as IVsShell;
-												if (shellPointer != null)
-												{
-													object installDir;
-													shellPointer.GetProperty((int)__VSSPROPID.VSSPROPID_InstallDirectory, out installDir);
-													mysInstallDirectory = (string)installDir;
-												}
-											}
-										}
-									}
-								}
-							}
-							finally
-							{
-								if (pvShell != IntPtr.Zero)
-								{
-									Marshal.Release(pvShell);
-								}
-								if (pvHier != IntPtr.Zero)
-								{
-									Marshal.Release(pvHier);
-								}
-							}
-						}
-					}
-				}
-				return mysInstallDirectory;
-			}
-		}
 		/// <summary>
 		/// Generate a dmd file for the current ims file contents. Note that this
 		/// takes a contents instead of a file name so that we don't have to save
@@ -151,55 +78,49 @@ namespace Neumont.Tools.Converters
 		/// <returns>Contents of the corresponding dmd file</returns>
 		private string GenerateDmd(string imsContents)
 		{
-			FileInfo imsFileInfo = null;
-			FileInfo dmdFileInfo = null;
-			string retVal = "";
-			try
+			Store store = CreateStore();
+			using (MemoryStream contentsStream = new MemoryStream())
 			{
-				// Generate temp files and names
-				imsFileInfo = new FileInfo(Path.GetTempFileName());
-				dmdFileInfo = new FileInfo(Path.GetTempFileName());
-				// Set the extensions to something the ImsToDmd tool will like.
-				// Changing the extension is a clear hack, but the chances of the other
-				// files existing are small. GetTempFileName should
-				// have overloads to take a prefix/extension, but it doesn't.
-				imsFileInfo.MoveTo(imsFileInfo.FullName + ".ims");
-				dmdFileInfo.MoveTo(dmdFileInfo.FullName + ModelFileExtension);
-
-				// Move the ims data into the temporary .ims file
-				FileStream fileStream = imsFileInfo.OpenWrite();
-				using (StreamWriter writer = new StreamWriter(fileStream))
+				using (StreamWriter writer = new StreamWriter(contentsStream))
 				{
+					// Get the contents into a stream for the ImsReader API
 					writer.Write(imsContents);
+					writer.Flush();
+					// Note: don't close the writer yet, we'll recycle the stream
+					
+					// Set the stream back to the beginning and read it in
+					contentsStream.Position = 0;
+					ImsReader.Load(store, contentsStream, false);
+
+					// Resuse the stream to save the store back into
+					contentsStream.Position = 0;
+					DmdWriter.Save(store, contentsStream);
+					// If the saved stream is smaller than the starting stream
+					// then the length will be wrong
+					contentsStream.SetLength(contentsStream.Position);
+
+					// Dump it back out as a string
+					contentsStream.Position = 0;
+					using (StreamReader reader = new StreamReader(contentsStream))
+					{
+						return reader.ReadToEnd();
+					}
 				}
-				fileStream.Close();
-
-				// Shell the process
-				Process proc = new Process();
-				ProcessStartInfo startInfo = proc.StartInfo;
-				startInfo.FileName = InstallDirectory + @"\IMSTODMD.exe";
-				startInfo.Arguments = string.Format(@"""{0}"" ""{1}""", imsFileInfo.FullName, dmdFileInfo.FullName);
-				startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-				proc.Start();
-				proc.WaitForExit();
-
-				// Retrieve the dmd data and return it
-				StreamReader reader = dmdFileInfo.OpenText();
-				retVal = reader.ReadToEnd();
-				reader.Close();
 			}
-			finally
+		}
+		private static Store CreateStore()
+		{
+			Store store = new Store();
+			Type[] substoreTypes = new Type[] { typeof(CoreDesignSurface), typeof(CoronaConcepts), typeof(PhoenixMetaModel) };
+			int substoreTypeCount = substoreTypes.Length;
+			store.LoadMetaModels(substoreTypes);
+			object[] createParams = new object[]{store};
+			Type[] ctorArgs = new Type[]{typeof(Store)};
+			for (int i = 0; i < substoreTypeCount; ++i)
 			{
-				if (imsFileInfo != null)
-				{
-					imsFileInfo.Delete();
-				}
-				if (imsFileInfo != null)
-				{
-					dmdFileInfo.Delete();
-				}
+				substoreTypes[i].GetConstructor(ctorArgs).Invoke(createParams);
 			}
-			return retVal;
+			return store;
 		}
 		#endregion // CustomTool Specific
 	}
