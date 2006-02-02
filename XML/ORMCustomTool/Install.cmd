bcec01c9dcda7b038aa9cbd4daa9d6a60a45298a
Kevin Owen <Kevin.Owen@neumont.edu>
2006-02-02 04:50:01 +0000

    Modified Setup and install.bat to install ORMCustomTool alongside the package DLL and register it with a CodeBase. Also added ORMCustomTool.pdb to Setup. refs #193
    SVN Revision: 438

--- XML/ORMCustomTool/Install.cmd
+++ XML/ORMCustomTool/Install.cmd
@@ -1,12 +1,14 @@
 @ECHO OFF
+SETLOCAL
 
-IF "%~dp1"=="" (GOTO:_Install "%DevEnvDir%") ELSE (GOTO:_Install %*)
+SET VSDir=%ProgramFiles%\Microsoft Visual Studio 8
+SET NORMADir=%ProgramFiles%\Neumont\ORM Architect for Visual Studio
 
-:_Install
-COPY /V /Y "%~dp0\bin\Neumont.Tools.ORM.ORMCustomTool.dll" "%~dp1\PrivateAssemblies\"
-COPY /V /Y "%~dp0\bin\Neumont.Tools.ORM.ORMCustomTool.pdb" "%~dp1\PrivateAssemblies\"
+DEL /F /Q "%VSDir%\Common7\IDE\PrivateAssemblies\Neumont.Tools.ORM.ORMCustomTool.*" 1>NUL 2>&1
+XCOPY /Y /D /V /Q "%~dp0\bin\Neumont.Tools.ORM.ORMCustomTool.dll" "%NORMADir%\bin\"
+XCOPY /Y /D /V /Q "%~dp0\bin\Neumont.Tools.ORM.ORMCustomTool.pdb" "%NORMADir%\bin\"
 
-REG QUERY HKCR\CLSID\{977BD01E-F2B4-4341-9C47-459420624A20} /ve 1>NUL 2>&1
+REG QUERY HKCR\CLSID\{977BD01E-F2B4-4341-9C47-459420624A20}\InprocServer32 /v "CodeBase" 1>NUL 2>&1
 IF ERRORLEVEL 1 (GOTO:_InstallReg) ELSE (GOTO:EOF)
 
 :_InstallReg
@@ -24,6 +26,7 @@ REG ADD HKLM\SOFTWARE\Classes\CLSID\{977BD01E-F2B4-4341-9C47-459420624A20} /f /v
 REG ADD HKLM\SOFTWARE\Classes\CLSID\{977BD01E-F2B4-4341-9C47-459420624A20}\InprocServer32 /f /ve /d "%SystemRoot%\System32\mscoree.dll"
 REG ADD HKLM\SOFTWARE\Classes\CLSID\{977BD01E-F2B4-4341-9C47-459420624A20}\InprocServer32 /f /v "ThreadingModel" /d "Both"
 REG ADD HKLM\SOFTWARE\Classes\CLSID\{977BD01E-F2B4-4341-9C47-459420624A20}\InprocServer32 /f /v "Class" /d "Neumont.Tools.ORM.ORMCustomTool.ORMCustomTool"
+REG ADD HKLM\SOFTWARE\Classes\CLSID\{977BD01E-F2B4-4341-9C47-459420624A20}\InprocServer32 /f /v "CodeBase" /d "%NORMADir%\bin\Neumont.Tools.ORM.ORMCustomTool.dll"
 REG ADD HKLM\SOFTWARE\Classes\CLSID\{977BD01E-F2B4-4341-9C47-459420624A20}\InprocServer32 /f /v "Assembly" /d "Neumont.Tools.ORM.ORMCustomTool, Version=1.0, Culture=neutral, PublicKeyToken=957d5b7d5e79e25f"
 GOTO:EOF
 
