9e585b48a4a4370f3f0f6e475bb4d21db2b6010a
Kevin Owen <Kevin.Owen@neumont.edu>
2006-02-25 02:33:40 +0000

    Added copyright notices to supporting tools. Added CPL.txt to Setup. Added setup.exe.manifest to Setup so that the DSL Tools bootstrapper will bind to the XP version of Common Controls if available. refs #213
    SVN Revision: 487

--- CleanDSLDM/MapIds.xslt
+++ CleanDSLDM/MapIds.xslt
@@ -1,12 +1,25 @@
-<?xml version="1.0" encoding="UTF-8" ?>
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+	Neumont Object Role Modeling Architect for Visual Studio
+
+	Copyright © Neumont University. All rights reserved.
+
+	The use and distribution terms for this software are covered by the
+	Common Public License 1.0 (http://opensource.org/licenses/cpl) which
+	can be found in the file CPL.txt at the root of this distribution.
+	By using this software in any fashion, you are agreeing to be bound by
+	the terms of this license.
+
+	You must not remove this notice, or any other, from this software.
+-->
 <xsl:stylesheet
 	version="1.0"
 	xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
 	xmlns:msxsl="urn:schemas-microsoft-com:xslt"
 	xmlns:idMap="id-map-extension"
 	xmlns:dmd="urn:schemas-microsoft-com:dmd"
-	exclude-result-prefixes="msxsl"
-	>
+	exclude-result-prefixes="msxsl">
+	<xsl:output method="xml" encoding="utf-8" indent="no"/>
 	<!-- Templates to duplicate structure with new Ids -->
 	<xsl:template match="*">
 		<xsl:copy>
--- ImsToDmdCustomTool/Properties/AssemblyInfo.cs
+++ ImsToDmdCustomTool/Properties/AssemblyInfo.cs
@@ -1,29 +1,14 @@
-﻿#region Using directives
-
+﻿using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-#endregion
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTitle("Neumont.Tools.Converters.dll")]
+[assembly: AssemblyDescription("IMStoDMDCustomTool for Neumont ORM Architect for Visual Studio")]
+[assembly: AssemblyCompany("Neumont University")]
+[assembly: AssemblyProduct("Neumont ORM Architect for Visual Studio")]
+[assembly: AssemblyCopyright("Copyright © Neumont University. All rights reserved.")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ImsToDmdCustomTool")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImsToDmdCustomTool")]
-[assembly: AssemblyCopyright("Copyright @  2005")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
