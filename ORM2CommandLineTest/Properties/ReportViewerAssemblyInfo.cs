8eb79877c6ccde0b72bd6d8bf5181bf8a2493dc4
Orion Buhler <Orion.Buhler@neumont.edu>
2006-09-11 21:55:07 +0000

    Missing file from [779] : ReportViewerAssemblyInfo.cs
    refs #284
    
    SVN Revision: 786

---
+++ ORM2CommandLineTest/Properties/ReportViewerAssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyTitle("ORMTestReportViewer.exe")]
+[assembly: AssemblyProduct("Neumont ORM Architect for Visual Studio")]
+[assembly: AssemblyDescription("Neumont ORM Architect for Visual Studio - ORM Unit Test Report Viewer")]
+[assembly: AssemblyCompany("Neumont University")]
+[assembly: AssemblyCopyright("Copyright © Neumont University. All rights reserved.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+//[assembly: AssemblyVersion("1.0.*")]
