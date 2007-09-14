89761e886211ab6f8065ea8db33e3d077376bc88
Matt Curland <Matt.Curland@neumont.edu>
2007-09-14 04:16:09 +0000

    Fixed misc build errors from [1131]. refs #223
    SVN Revision: 1133

---
+++ TestSuites/AutomationTestSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyTitle("AutomationTestSample.dll")]
+[assembly: AssemblyProduct("Neumont ORM Architect for Visual Studio")]
+[assembly: AssemblyDescription("Neumont ORM Architect for Visual Studio - Automation Testing Sample")]
+[assembly: AssemblyCompany("Neumont University")]
+[assembly: AssemblyCopyright("Copyright © Neumont University. All rights reserved.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+//[assembly: AssemblyVersion("1.0.*")]
