08089145c6b3a2ef27733923acf6f35e731bd161
Matthew Curland <mcurland@live.com>
2020-05-31 16:10:49 -0600

    VS2012 and VS2013 builds need vsct.exe on path
    This is the same path extension code used in environment setup for
    later VS versions. I do not know what I changed cause the failure
    or why it works in previous versions (VS2005-VS2010).

--- VS2013.bat
+++ VS2013.bat
@@ -1,1 +1,27 @@
 @CALL "%~dp0VSVer.bat" 2013
+
+::Add SDK tools to path if vsct cannot be found, fails to compile in VS2013 otherwise
+set SDKVSCT=
+for %%i in (vsct.exe) do (set EXISTINGVSCT=%%~s$PATH:i)
+if '%EXISTINGVSCT%'=='' (
+	CALL:GETSHORTPATH "SDKVSCT" "%VSSDK120INSTALL%\VisualStudioIntegration\Tools\Bin"
+)
+if NOT '%SDKVSCT%'=='' (
+	CALL:EXTENDPATH "%SDKVSCT%"
+)
+CALL:SETVAR "VSSdkInstall" "%VSSDK120INSTALL%"
+SET FOUNDVSCT=
+SET SDKVSCT=
+goto:EOF
+
+:GETSHORTPATH
+SET %~1=%~s2
+GOTO:EOF
+
+:EXTENDPATH
+SET PATH=%PATH%;%~1
+GOTO:EOF
+
+:SETVAR
+SET %~1=%~2
+GOTO:EOF
