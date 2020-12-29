42490006a548091420a434fb2f0151063209c85a
Matthew Curland <mcurland@live.com>
2020-12-29 01:04:36 -0700

    Change to git-based file versioning
    The quarter-and-day based versioning system recently rolled over to
    100 (for example Christmas 2020 was revision 10091). This five-digit
    revision number was no longer compatible with the WIX-based setup
    identifier generation thereby blocking setup from being built for
    VS2015 and earlier versions of the product.
    
    A ground-up rewrite of the VersionGenerator utility was done to
    support this versioning approach. There are 4 enhancements:
    
    1) The GitCommand and GitCommandArgs options have been added to
    specify an external process to invoke to get the git-based file
    version. This currently calls 'NORMAGitVer full .', which outputs
    the expected version information.
    
    2) VersionGenerator.exe no longer hard-codes specific files to
    generate. The output files are now part of the configuration file in
    the form of templates with replacement fields. A template element has
    a format string as the contents of the element. The attributes on
    the template element are output (the file name to write) and numeric
    values for each of the available replacement fields, indicating the
    replacement index to use in the format string. Available fields are
    major, minor, build, revision, hash, yearMonth, buildType, and
    warning. The Version.bat, Version.wxi and Version.cs files are now
    templated in the config file instead of hard-wired into the utility.
    
    3) Since git versioning can change without touching the day or
    config files there was a need of a separate file to indicate the
    state when the files were last generated. This is set with the
    templateSettings@last attribute. This file is not checked in and
    has been added to .gitignore.
    
    4) VersionGenerator.exe can optionally take an argument indicating
    the file path to an alternate configuration file. This allows
    extension developers to use this utility to version assemblies using
    the same approach taken here and/or to reference the NORMA version
    information. The current directory is also set to the location of the
    alternate configuration file.
    
    With the git-based versioning, the MatchBuild = true setting for the
    NORMAExtensionCompatibility attribute will be less useful because the
    NORMA core and extension versions will be based on different git
    histories. This means the NORMA build number will need to be
    dynamically extracted from the NORMA repository to enable extension
    compatibility checks. The VersionGenerator utility config-based
    templates and custom configuration files enable this scenario.

--- NORMAGitVer.bat
+++ NORMAGitVer.bat
@@ -1,10 +1,11 @@
 @ECHO OFF
 SETLOCAL
-FOR /F "usebackq delims=v.-g tokens=1,2,3,4" %%i in (`git -C "%2" describe --first-parent --long %3`) DO (
+FOR /F "usebackq delims=v.-g tokens=1,2,3,4,5" %%i in (`git -C "%2" describe --first-parent --long %3`) DO (
 SET GitMajor=%%i
 SET GitMinor=%%j
 SET GitSub=%%k
 SET GitUpdate=%%l
+SET GitHash=%%m
 )
 SET PadSub=00%GitSub%
 REM Put a 3 in front of the tagged update number. This distinguishes the git tag
@@ -15,6 +16,9 @@ IF "%1"=="all" (
 )
 IF "%1"=="" (
 @ECHO %GitMajor%.%GitMinor%.3%PadSub:~-3%.%GitUpdate%
+)
+IF "%1"=="full" (
+@ECHO %GitMajor%.%GitMinor%.3%PadSub:~-3%.%GitUpdate%-g%GitHash%
 )
 IF "%1"=="major" (
 @ECHO %GitMajor%
@@ -27,4 +31,7 @@ IF "%1"=="build" (
 )
 IF "%1"=="revision" (
 @ECHO %GitUpdate%
+)
+IF "%1"=="hash" (
+@ECHO %GitHash%
 )
--- Tools/VersionGenerator/Config.cs
+++ Tools/VersionGenerator/Config.cs
@@ -3,6 +3,7 @@
 * Neumont Object-Role Modeling Architect for Visual Studio                 *
 *                                                                          *
 * Copyright © Neumont University. All rights reserved.                     *
+* Copyright © ORM Solutions, LLC. All rights reserved.                     *
 *                                                                          *
 * The use and distribution terms for this software are covered by the      *
 * Common Public License 1.0 (http://opensource.org/licenses/cpl) which     *
@@ -15,9 +16,11 @@
 #endregion
 
 using System;
+using System.Collections.Specialized;
 using System.Configuration;
+using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.Text;
+using System.Xml;
 
 namespace Neumont.Tools.ORM.SDK
 {
@@ -25,13 +28,298 @@ namespace Neumont.Tools.ORM.SDK
 	{
 		private static class Config
 		{
-			public static readonly int MajorVersion = int.Parse(ConfigurationManager.AppSettings["MajorVersion"]);
-			public static readonly int MinorVersion = int.Parse(ConfigurationManager.AppSettings["MinorVersion"]);
-			public static readonly DateTime ReleaseYearMonth = DateTime.ParseExact(ConfigurationManager.AppSettings["ReleaseYearMonth"], "yyyy-MM", CultureInfo.InvariantCulture, DateTimeStyles.NoCurrentDateDefault);
-			public static readonly string ReleaseType = ConfigurationManager.AppSettings["ReleaseType"];
-			public static readonly DateTime RevisionStartYearMonth = DateTime.ParseExact(ConfigurationManager.AppSettings["RevisionStartYearMonth"], "yyyy-MM", CultureInfo.InvariantCulture, DateTimeStyles.NoCurrentDateDefault);
-			private static readonly string CountQuartersFromYearMonthString = ConfigurationManager.AppSettings["CountQuartersFromYearMonth"];
-			public static readonly DateTime CountQuartersFromYearMonth = string.IsNullOrEmpty(CountQuartersFromYearMonthString) ? DateTime.Today.AddYears(1) : DateTime.ParseExact(CountQuartersFromYearMonthString, "yyyy-MM", CultureInfo.InvariantCulture, DateTimeStyles.NoCurrentDateDefault);
+			// Pull in data on first access, not readonly static, to allow for dynamic config swapping
+			private static Configuration _customConfig;
+			private static bool _init;
+			private static int _majorVersion;
+			private static int _minorVersion;
+			private static DateTime _releaseYearMonth;
+			private static string _releaseType;
+			private static DateTime _revisionStartYearMonth;
+			private static DateTime _countQuartersFromYearMonth;
+			private static string _gitCommand;
+			private static string _gitCommandArgs;
+
+			private static void EnsureInit()
+			{
+				if (!_init)
+				{
+					_init = true;
+					NameValueCollection defaultSettings;
+					KeyValueConfigurationCollection customSettings;
+					if (_customConfig != null)
+					{
+						defaultSettings = null;
+						customSettings = _customConfig.AppSettings.Settings;
+					}
+					else
+					{
+						defaultSettings = ConfigurationManager.AppSettings;
+						customSettings = null;
+					}
+					_majorVersion = int.Parse(GetSetting("MajorVersion", defaultSettings, customSettings));
+					_minorVersion = int.Parse(GetSetting("MinorVersion", defaultSettings, customSettings));
+					_releaseYearMonth = DateTime.ParseExact(GetSetting("ReleaseYearMonth", defaultSettings, customSettings), "yyyy-MM", CultureInfo.InvariantCulture, DateTimeStyles.NoCurrentDateDefault);
+					_releaseType = GetSetting("ReleaseType", defaultSettings, customSettings);
+					_revisionStartYearMonth = DateTime.ParseExact(GetSetting("RevisionStartYearMonth", defaultSettings, customSettings), "yyyy-MM", CultureInfo.InvariantCulture, DateTimeStyles.NoCurrentDateDefault);
+					string countQuartersFromYearMonthString = GetSetting("CountQuartersFromYearMonth", defaultSettings, customSettings);
+					_countQuartersFromYearMonth = string.IsNullOrEmpty(countQuartersFromYearMonthString) ? DateTime.Today.AddYears(1) : DateTime.ParseExact(countQuartersFromYearMonthString, "yyyy-MM", CultureInfo.InvariantCulture, DateTimeStyles.NoCurrentDateDefault);
+					_gitCommand = GetSetting("GitCommand", defaultSettings, customSettings);
+					_gitCommandArgs = GetSetting("GitCommandArgs", defaultSettings, customSettings);
+				}
+			}
+			private static string GetSetting(string name, NameValueCollection defaultSettings, KeyValueConfigurationCollection customSettings)
+			{
+				return defaultSettings != null ? defaultSettings[name] : customSettings[name].Value;
+			}
+
+			public static Configuration CustomConfig
+			{
+				get
+				{
+					return _customConfig;
+				}
+				set
+				{
+					_init = false;
+					_customConfig = value;
+				}
+			}
+
+			public static int MajorVersion
+			{
+				get
+				{
+					EnsureInit();
+					return _majorVersion;
+				}
+			}
+
+			public static int MinorVersion
+			{
+				get
+				{
+					EnsureInit();
+					return _minorVersion;
+				}
+			}
+
+			public static DateTime ReleaseYearMonth
+			{
+				get
+				{
+					EnsureInit();
+					return _releaseYearMonth;
+				}
+			}
+
+			public static string ReleaseType
+			{
+				get
+				{
+					EnsureInit();
+					return _releaseType;
+				}
+			}
+
+			public static DateTime RevisionStartYearMonth
+			{
+				get
+				{
+					EnsureInit();
+					return _revisionStartYearMonth;
+				}
+			}
+
+			public static DateTime CountQuartersFromYearMonth
+			{
+				get
+				{
+					EnsureInit();
+					return _countQuartersFromYearMonth;
+				}
+			}
+
+			public static string GitCommand
+			{
+				get
+				{
+					EnsureInit();
+					return _gitCommand;
+				}
+			}
+			public static string GitCommandArgs
+			{
+				get
+				{
+					EnsureInit();
+					return _gitCommandArgs;
+				}
+			}
+		}
+		private class TemplateConfigurationSection : ConfigurationSection
+		{
+			[ConfigurationProperty("last")]
+			public string LastGenerated
+			{
+				get { return this["last"] as string; }
+				set { this["last"] = value; }
+			}
+
+			[ConfigurationProperty("templates")]
+			[SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly", Justification = "configuration element collections cannot be read-only")]
+			[ConfigurationCollection(typeof(TemplateCollection))]
+			public TemplateCollection Templates
+			{
+				get { return this["templates"] as TemplateCollection; }
+				set { this["templates"] = value; }
+
+			}
+		}
+		private class TemplateCollection : ConfigurationElementCollection
+		{
+			public override ConfigurationElementCollectionType CollectionType
+			{
+				get { return ConfigurationElementCollectionType.BasicMap; }
+			}
+
+			protected override bool IsElementName(string elementName)
+			{
+				return elementName == "template";
+			}
+
+			protected override ConfigurationElement CreateNewElement()
+			{
+				throw new InvalidOperationException("Unspecified element name.");
+			}
+
+			protected override ConfigurationElement CreateNewElement(string elementName)
+			{
+				switch (elementName)
+				{
+					case "template":
+						return new TemplateElement();
+
+					default:
+						throw new InvalidOperationException("Unknown element name.");
+				}
+			}
+
+			protected override object GetElementKey(ConfigurationElement element)
+			{
+				TemplateElement template = element as TemplateElement;
+				if (template == null)
+				{
+					throw new InvalidOperationException(string.Format("Cannot derive key for element: {0}", element.GetType().FullName));
+				}
+				return template.Output;
+			}
+		}
+		private class TemplateElement : ConfigurationElement
+		{
+			private string _content;
+			[ConfigurationProperty("output")]
+			public string Output
+			{
+				get { return this["output"] as string; }
+				set { this["output"] = value; }
+			}
+			[ConfigurationProperty("major")]
+			public int Major
+			{
+				get
+				{
+					int result;
+					return int.TryParse(this["major"] as string, out result) ? result : -1;
+				}
+				set { this["major"] = value.ToString(); }
+			}
+			[ConfigurationProperty("minor")]
+			public int Minor
+			{
+				get
+				{
+					int result;
+					return int.TryParse(this["minor"] as string, out result) ? result : -1;
+				}
+				set { this["minor"] = value.ToString(); }
+			}
+			[ConfigurationProperty("build")]
+			public int Build
+			{
+				get
+				{
+					int result;
+					return int.TryParse(this["build"] as string, out result) ? result : -1;
+				}
+				set { this["build"] = value.ToString(); }
+			}
+			[ConfigurationProperty("revision")]
+			public int Revision
+			{
+				get
+				{
+					int result;
+					return int.TryParse(this["revision"] as string, out result) ? result : -1;
+				}
+				set { this["revision"] = value.ToString(); }
+			}
+			[ConfigurationProperty("hash")]
+			public int Hash
+			{
+				get
+				{
+					int result;
+					return int.TryParse(this["hash"] as string, out result) ? result : -1;
+				}
+				set { this["hash"] = value.ToString(); }
+			}
+			[ConfigurationProperty("yearMonth")]
+			public int YearMonth
+			{
+				get
+				{
+					int result;
+					return int.TryParse(this["yearMonth"] as string, out result) ? result : -1;
+				}
+				set { this["yearMonth"] = value.ToString(); }
+			}
+			[ConfigurationProperty("buildType")]
+			public int BuildType
+			{
+				get
+				{
+					int result;
+					return int.TryParse(this["buildType"] as string, out result) ? result : -1;
+				}
+				set { this["buildType"] = value.ToString(); }
+			}
+			[ConfigurationProperty("warning")]
+			public int Warning
+			{
+				get
+				{
+					int result;
+					return int.TryParse(this["warning"] as string, out result) ? result : -1;
+				}
+				set { this["warning"] = value.ToString(); }
+			}
+			public string Content
+			{
+				get { return _content; }
+				set { _content = value; }
+			}
+			protected override void DeserializeElement(XmlReader reader, bool serializeCollectionKey)
+			{
+				//First get the attributes
+				for (int i = 0, count = reader.AttributeCount; i < count; ++i)
+				{
+					reader.MoveToAttribute(i);
+					this[reader.Name] = reader.Value;
+				}
+				reader.MoveToElement();
+				_content = reader.ReadElementContentAsString();
+			}
 		}
 	}
 }
